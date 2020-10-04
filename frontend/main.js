86d2e000375b83dab0b61ef92f832137cfbe9376
Rafael Gonzaga Brito <rafaelgonzagabrito@gmail.com>
2020-10-04 17:23:56 -0300

    Update main.js

---
+++ frontend/main.js
@@ -0,0 +1,9 @@
+function enter() {
+            
+}
+function clicar(num) {
+    document.getElementById("display").value += num
+}
+function resetar() {
+    document.getElementById("display").value = ''
+}
